603cf24088c35c5aef0cba8c6c956ad14df3449e
Isak2003 <70603096+snurkeburk@users.noreply.github.com>
2020-11-30 14:38:12 +0100

    fixed hidden stuff (now appears in the right order and correcly everytime), added mutible checkboxes for every class, added textarea which only appears when you select 1 or more classes, fixed JS errors

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
 
 document .querySelector(".teknikCiv") .addEventListener("click", toggleDivDisplay);
+document .querySelector(".teknikSpel") .addEventListener("click", toggleDivDisplay);
+document .querySelector(".El") .addEventListener("click", toggleDivDisplay);
+document .querySelector(".Estet") .addEventListener("click", toggleDivDisplay);
+
 function toggleDivDisplay() {
-   var x = document.querySelector(".div-visible");
-   if (x.style.display === "block") {
-      x.style.display = "none";
+   var x = document.querySelector(".div-visible-civ");
+
+   if(x.clicked == true){
+      x.style.display = "block";
    } else {
       x.style.display = "block";
    }
+
+
+
+// 
+document .querySelector(".checkboxEttwebb") .addEventListener("click", toggleDivDisplay);
+document .querySelector(".checkboxEttdao") .addEventListener("click", toggleDivDisplay);
+document .querySelector(".checkboxEttfys") .addEventListener("click", toggleDivDisplay);
+document .querySelector(".checkboxEttmat") .addEventListener("click", toggleDivDisplay);
+document .querySelector(".checkboxEttpro") .addEventListener("click", toggleDivDisplay);
+document .querySelector(".checkboxEttsve") .addEventListener("click", toggleDivDisplay);
+function toggleDivDisplay() {
+   var k = document.querySelector(".ett");
+   var y = document.querySelector("#två");
+
+   if(k.clicked == true){
+      k.style.display = "none";
+      y.style.display = "none";
+   } else {
+      k.style.display = "block";
+      y.style.display = "block";
+
+   }
+  
+}
 }
