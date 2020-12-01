ef03a6a498e55f31803c9e6e38389cbd8c754864
Isak2003 <70603096+snurkeburk@users.noreply.github.com>
2020-12-01 12:43:58 +0100

    added :focus on "utveckla" and :valid + :invalid on email box. Added so that the OBS text dissapears after you select at least 1 favourite

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+//Detta är till för att man ska displaya ämnen efter att ha valt inriktning
 document .querySelector(".teknikCiv") .addEventListener("click", toggleDivDisplay);
 document .querySelector(".teknikSpel") .addEventListener("click", toggleDivDisplay);
 document .querySelector(".El") .addEventListener("click", toggleDivDisplay);
@@ -13,9 +13,9 @@ function toggleDivDisplay() {
       x.style.display = "block";
    }
 
+//-----------------------------------------------------------------------------//
 
-
-// 
+//Detta är till för att displaya "utveckla varför" efter att man har valt minst en klass 
 document .querySelector(".checkboxEttwebb") .addEventListener("click", toggleDivDisplay);
 document .querySelector(".checkboxEttdao") .addEventListener("click", toggleDivDisplay);
 document .querySelector(".checkboxEttfys") .addEventListener("click", toggleDivDisplay);
@@ -25,15 +25,20 @@ document .querySelector(".checkboxEttsve") .addEventListener("click", toggleDivD
 function toggleDivDisplay() {
    var k = document.querySelector(".ett");
    var y = document.querySelector("#två");
+   var h = document.querySelector("#OBS");
 
    if(k.clicked == true){
       k.style.display = "none";
       y.style.display = "none";
+      h.style.dispaly = "none";
    } else {
       k.style.display = "block";
       y.style.display = "block";
+      h.style.display = "none";
 
    }
   
 }
 }
+//-----------------------------------------------------------------------------//
+
